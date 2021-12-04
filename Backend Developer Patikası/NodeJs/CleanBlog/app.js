3e00854cf438da3a48369d44712fee6a0d31244b
Yunus Can Kuyumcu <65031891+cnkymc@users.noreply.github.com>
2021-12-04 23:01:06 +0300

    Upgrade

--- Backend Developer Patikası/NodeJs/CleanBlog/app.js
+++ Backend Developer Patikası/NodeJs/CleanBlog/app.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+
 const app = express();
 
+//TEMPLATE ENGİNE
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 5000;
